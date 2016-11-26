01a5acb418beb0266cc58f2ec5577ffa03703474
cqc <1016903103@qq.com>
2016-11-26 17:34:28 +0800

    add auth attempt

---
+++ src/Auth.php
@@ -0,0 +1,32 @@
+<?php
+namespace Germey\Support;
+
+use Illuminate\Support\Facades\Hash;
+use Models\User;
+
+class Auth
+{
+	/**
+	 * @param $email
+	 * @param $password
+	 * @return bool
+	 */
+	public function attempt($username, $password)
+	{
+		if (is_email($username)) {
+			$credentials = ['email' => $username];
+		} else if (is_phone($username)) {
+			$credentials = ['phone' => $username];
+		} else {
+			$credentials = ['name' => $username];
+		}
+		$user = User::where($credentials)->first();
+		if (! $user) {
+			return false;
+		}
+		if (Hash::check($password, $user->password)) {
+			return true;
+		}
+		return false;
+	}
+}
